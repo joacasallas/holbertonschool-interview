109ce953d705fe7f40d95cd605d6187e9d956687
joacasallas <joacasallas@gmail.com>
2022-08-09 13:13:27 -0500

    update

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,10 +2,17 @@
 """Write a method that determines if all the boxes can be opened"""
 def canUnlockAll(boxes):
     """function to check if key exists"""
-    key = 0
-    while boxes:
-        key = key + 1
-    if key == (len(boxes) -1):
-        return true
-    else:
-        return false
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
+     
